30c0ade68581f64800a3274c4f64a056b4dd35f6
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-08-19 18:10:54 +0530

    csv data insertion in database using Mongodb..Done

--- create_class.rb
+++ create_class.rb
@@ -1,31 +1,33 @@
 require_relative 'file_operations.rb'
 
+require 'mongo'
+
 class CreateClass	
 	include FileOperation
 	
 	def self.read_csv_file
-		puts @file_name
+		puts "Reading #{@file_name}.csv file...."
 	end
 
 	def self.create_class(class_name)
-		klass= Object.const_set "#{class_name}",Class.new
+		Object.const_set "#{class_name}",Class.new
 		@data[0].each  do |x|
 			x.gsub!(/\s/,'_')
 			x.downcase
 		end
+		
 		class_properties=@data[0]
+		@clas_properties=class_properties
 		eval("#{class_name}").class_eval do
 			attr_accessor *class_properties
 		end
+
 		object_counter = 0
 		@data.each do |elements|
 			if object_counter >=1
 				#element_counter = 0
 				Object.const_set "#{class_name}_object_#{object_counter}", eval("#{class_name}.new")
 					class_properties.zip(elements).each do |var,value|
-						puts Object.const_defined? "#{class_name}_object_#{object_counter}"
-						puts "#{var}.....#{value}......#{class_name}_object_#{object_counter}"
-						puts("#{class_name}_object_#{object_counter}.#{var}=value")
 						eval("#{class_name}_object_#{object_counter}.#{var}=value")
 					end
 				#end
@@ -34,12 +36,31 @@ class CreateClass
 		end
 	end
 
+	def self.insert_values class_name
+		mongo_client = Mongo::MongoClient.new("localhost",27017)
+		@db = mongo_client.db("my_database")
+		collection_name=class_name.downcase
+		collection_name[collection_name.size]='s'
+		coll=@db.collection(collection_name)
+		puts "Collection name : #{collection_name}"
+		counter=0
+		@data.each do |elements|
+			if counter>=1
+				array=@clas_properties.zip(elements)
+				hash=Hash[*array.flatten]
+				coll.insert(hash)
+			end
+			counter+=1
+		end
+	end	
+
 	def self.set_class
 		@csv_file_name="#{@file_name}.csv"
 		class_name=@file_name.capitalize
 		class_name[class_name.size-1]=''
 		@data=CSV.read(@csv_file_name,"r")
-		CreateClass.create_class(class_name)
+		CreateClass.create_class class_name
+		self.insert_values class_name
 	end
 end
 CreateClass.run_file
